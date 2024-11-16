807f9da8406c9813c79b4a59a2150b40d937d7c2
Oleg <109240899+Volmol16@users.noreply.github.com>
2024-11-16 09:32:10 +0300

    add-project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="">
+  
+</style>
