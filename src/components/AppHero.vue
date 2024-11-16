1007d390096ef0180095708f1a4884d11ffe63d0
Sergej Cybulnikov <sergejcybulnikov911@gmail.com>
2024-11-16 17:50:29 +0300

    AddHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,14 +1,66 @@
 <template>
-    <div class="hero-info">
+    <section class="hero">
+        <div class="hero-info">
         <h1 class="hero-title">
-            ChatGPT, but with your own data
+            ChatGPT, but with your own <span>Data</span>
         </h1>
         <p class="hero-description">
             Effortlessly create AI chatbots tailored to your data, documents, or files, and experience limitless general
             chatting with ChatGPT. Boost your productivity, learning, and research with the power of AI at your
             fingertips
         </p>
+        <div class="get-started">
+            <button>
+                Get started ->
+            </button>
+            <div class="get-started-avatar">
+                <img src="/img/Avatar-1.jpg" alt="avatar">
+                <img src="/img/Avatar-2.jpg" alt="avatar">
+                <img src="/img/Avatar-3.jpg" alt="avatar">
+            </div>
+            <p>
+                Join with 10,000+ users
+            </p>
+        </div>
     </div>
+    <div class="hero-image">
+        <img src="/img/cuate.png" width="593" height="525" alt="">
+    </div>
+    </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .hero {
+        margin-top: 108px;
+        display: flex;
+        align-items: center;
+    }
+
+    .hero-title {
+        margin-bottom: 40px;
+        width: 706px;
+        font-size: 70px;
+        font-weight: 800;
+        line-height: 84px;
+        color: #fff;
+    }
+
+    .hero-title span {
+        font-size: 70px;
+        font-weight: 800;
+        line-height: 84px;
+        color: #00ADB5;
+    }
+
+    .hero-description {
+        margin-bottom: 56px;
+        width: 545px;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 24px;
+        opacity: 60%;
+        color: #fff;
+    }
+
+   
+</style>
